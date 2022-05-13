0594328c8b76a6195b4b446c19455cdea758ce76
kapil.soni <kapil.soni@bacancy.com>
2022-05-13 17:29:38 +0530

    #release/version_0.0 - added code in streams.js

--- myChat/static/js/streams.js
+++ myChat/static/js/streams.js
@@ -1,3 +1,12 @@
 const APP_ID = "2a437b8895d249889b9ba902153f6bc5"
 const CHANNEL = "main"
 const TOKEN = "0062a437b8895d249889b9ba902153f6bc5IABhVw6jxziIxvu+Q+LQp3PJm5dt8d1rWg4fU+pg5bo/umTNKL8AAAAAEAA5DUG6Oo5/YgEAAQA6jn9i"
+
+const client = AgoraRTC.createClient({mode:'rtc',codec:'vp8'})
+
+let localTracks = []
+let remoteUsers = {}
+
+let joinAndDisplayLocalStreams = async () => {
+    await client.join(APP_ID,CHANNEL,TOKEN,null)
+}
